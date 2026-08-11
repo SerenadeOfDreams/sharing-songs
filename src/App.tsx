a8881080324f6ae13878c953f4d11d26dad97c30
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2026-08-11 18:00:25 -0300

    refactor: using a RouterProvider component with the project router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 function App() {
   return (
     <>
-      <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
