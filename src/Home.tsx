b9ef6699d336e84d3f0d308775e5203d579f13cd
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2026-08-11 17:57:17 -0300

    feat: component Home.tsx created.

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+export function Home() {
+  return (
+    <main className="mx-auto max-w-xl px-4 py-10 sm:py-14">
+      <p>
+        Olá. O intuito deste site é compartilhar com as pessoas as músicas que
+        eu gosto e praticar meu aprendizado de desenvolvimento web com React e
+        Typescript. <br />
+        <br /> Nas páginas de bandas e músicas, tentarei deixar links para mais
+        de uma plataforma onde você pode ouvir.
+      </p>
+    </main>
+  );
+}
